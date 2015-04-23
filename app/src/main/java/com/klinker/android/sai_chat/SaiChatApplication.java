49444c94d7ae74e065d5457dc593b24cff368711
Luke Klinker <lklinker1@gmail.com>
2015-04-22 20:41:46 -0500

    initial commit

---
+++ app/src/main/java/com/klinker/android/sai_chat/SaiChatApplication.java
@@ -0,0 +1,13 @@
+package com.klinker.android.sai_chat;
+
+import android.app.Application;
+
+public class SaiChatApplication extends Application {
+
+    /**
+     * In this class, we could do things like cache management or some data loading.
+     * This app doesn't need any of that of course, but we created it and put a
+     * reference to it in the AndroidManifest.xml
+     */
+
+}
